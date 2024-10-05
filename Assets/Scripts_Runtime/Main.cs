fda1231a404ddceb977dd7c0b4c7fde5fe3ef73a
lazyihui <chengbaozi6@qq.com>
2024-10-05 23:10:55 +0800

    基本配置

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VR
+{
+
+    public class Main : MonoBehaviour
+    {
+        void Awake()
+        {
+            Debug.Log("Main.Awake");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
